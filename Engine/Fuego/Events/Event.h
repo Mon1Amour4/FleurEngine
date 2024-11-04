b117a900a1624574478a9dd3420329fbceae1ca3
LtSnail <frogressivemetal@gmail.com>
2024-11-04 21:57:35 +0100

    Revert "[Win] Change EventCategory Macro for similarity with EventType Macro"
    This reverts commit 6f8418fcf7384d81ac57ae91fc052503eb951376.
    
    # Conflicts:
    #	Engine/Fuego/Events/Event.h

--- Engine/Fuego/Events/Event.h
+++ Engine/Fuego/Events/Event.h
@@ -48,7 +48,7 @@ namespace Fuego
                                 EventType GetEventType() const override { return GetStaticType(); }\
                                 const char* GetName() const override { return #type; }
 
-#define EVENT_CLASS_CATEGORY(category) int GetCategoryFlags() const override { return EventCategory##category; }
+#define EVENT_CLASS_CATEGORY(category) int GetCategoryFlags() const override { return category; }
 
     class FUEGO_API Event
     {
