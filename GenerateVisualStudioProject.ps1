d9aebef1558c675199fc6b1abbbf5261d3c2331c
Oleg Cherkunov <mon1amour4@gmail.com>
2024-11-05 15:36:55 +0800

    [Win] Add Git command to Init Update Git SubModules

--- GenerateVisualStudioProject.ps1
+++ GenerateVisualStudioProject.ps1
@@ -1,6 +1,9 @@
 $rootFolder = "$PSScriptRoot"
 $buildDir = Join-Path -Path $rootFolder -ChildPath "build\win"
 
+git -C $rootFolder submodule init
+git -C $rootFolder submodule update
+
 if (-Not (Test-Path $buildDir)) {
     New-Item -ItemType Directory -Path $buildDir
 }
