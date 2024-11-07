46c83bd70f8f62159c4fe6f1ac1ab1a394a6941b
Oleg Cherkunov <mon1amour4@gmail.com>
2024-11-07 13:25:20 +0800

    [Win] Draw an empty window

--- Engine/Fuego/Windows/WindowWin.h
+++ Engine/Fuego/Windows/WindowWin.h
@@ -4,6 +4,8 @@
 
 namespace Fuego
 {
+    LRESULT CALLBACK WindowProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
 	class WindowWin : public Window
 	{
 	public:
@@ -16,14 +18,17 @@ namespace Fuego
 
         virtual void SetVSync(bool enabled) override;
         virtual bool IsVSync() const override;
-
+       
     private:
         void Init(const WindowProps& props, EventQueue& eventQueue);
         void Shutdown();
 
         // Window handle
         HANDLE m_Window;
-
+        HINSTANCE m_HInstance;  // Relates to the Application
+        HWND m_Hwnd;            // Relates to Actual Window instance
         WindowProps m_Props;
+
+        static const TCHAR* APP_WINDOW_CLASS_NAME;
 	};
 }
