64b7e82b08a610fe2a1ed56cce17021b3aa3951a
LtSnail <frogressivemetal@gmail.com>
2024-11-11 19:46:42 +0100

    Add ifdef to forbid incorrect log usage

--- Engine/Fuego/Log.h
+++ Engine/Fuego/Log.h
@@ -25,16 +25,18 @@ class Log
 };
 }  // namespace Fuego
 
-// Core Log macros.
+#if defined(FUEGO_BUILD_LIB)
 #define FU_CORE_TRACE(...) ::Fuego::Log::GetCoreLogger()->trace(__VA_ARGS__)
 #define FU_CORE_INFO(...) ::Fuego::Log::GetCoreLogger()->info(__VA_ARGS__)
 #define FU_CORE_WARN(...) ::Fuego::Log::GetCoreLogger()->warn(__VA_ARGS__)
 #define FU_CORE_ERROR(...) ::Fuego::Log::GetCoreLogger()->error(__VA_ARGS__)
 #define FU_CORE_FATAL(...) ::Fuego::Log::GetCoreLogger()->fatal(__VA_ARGS__)
+#endif
 
-// Client Log macros.
+#if !defined(FUEGO_BUILD_LIB)
 #define FU_TRACE(...) ::Fuego::Log::GetClientLogger()->trace(__VA_ARGS__)
 #define FU_INFO(...) ::Fuego::Log::GetClientLogger()->info(__VA_ARGS__)
 #define FU_WARN(...) ::Fuego::Log::GetClientLogger()->warn(__VA_ARGS__)
 #define FU_ERROR(...) ::Fuego::Log::GetClientLogger()->error(__VA_ARGS__)
 #define FU_FATAL(...) ::Fuego::Log::GetClientLogger()->fatal(__VA_ARGS__)
+#endif
