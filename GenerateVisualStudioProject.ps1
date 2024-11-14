d49eabade1ece94e4ed53c127189ee98555a3f3d
a.zyktin <zyktin.a.a@gmail.com>
2024-11-15 03:26:42 +0800

    [Win] Add return to previous folder

--- GenerateVisualStudioProject.ps1
+++ GenerateVisualStudioProject.ps1
@@ -8,6 +8,9 @@ if (-Not (Test-Path $buildDir)) {
     New-Item -ItemType Directory -Path $buildDir
 }
 
+$currentDir = Get-Location
 Set-Location $buildDir
 
 cmake -S $rootFolder -B . -G "Visual Studio 17 2022" 
+
+Set-Location $currentDir
