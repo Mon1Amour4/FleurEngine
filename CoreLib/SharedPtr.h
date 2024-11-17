3fc8a60136195592c765b5ba308b2b1675d559ac
a.zyktin <zyktin.a.a@gmail.com>
2024-11-18 03:53:15 +0800

    Add basic (empty) SharedPtr class

---
+++ CoreLib/SharedPtr.h
@@ -0,0 +1,14 @@
+﻿#pragma once
+
+
+#include "CoreLibConcepts.h"
+
+#include <Engine/Fuego/Core.h>
+
+
+namespace Fuego
+{
+    template<class T, class Deleter> requires IsDefaultCompatibleDeleter<T, Deleter>
+    class FUEGO_API SharedPtr
+    {};
+}
