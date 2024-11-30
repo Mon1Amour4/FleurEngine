ac6c5664388cd73d881338f1b075c63788385283
a.zyktin <zyktin.a.a@gmail.com>
2024-12-01 00:24:36 +0800

    Add default allocator

---
+++ CoreLib/DefaultAllocator.cpp
@@ -0,0 +1,14 @@
+﻿#include "DefaultAllocator.h"
+
+#include <Core.h>
+
+uint8_t* Fuego::DefaultAllocator::allocate(size_t size) noexcept
+{
+    return static_cast<uint8_t*>(::operator new[](size, std::nothrow));
+}
+
+void Fuego::DefaultAllocator::deallocate(uint8_t* ptr, size_t size) noexcept
+{
+    UNUSED(size);
+    ::operator delete[](ptr, std::nothrow);
+}
---
+++ CoreLib/DefaultAllocator.h
@@ -0,0 +1,13 @@
+﻿#pragma once
+
+#include <cstdint>
+
+namespace Fuego
+{
+class DefaultAllocator
+{
+public:
+    uint8_t* allocate(size_t size) noexcept;
+    void deallocate(uint8_t* ptr, size_t size) noexcept;
+};
+}  // namespace Fuego
