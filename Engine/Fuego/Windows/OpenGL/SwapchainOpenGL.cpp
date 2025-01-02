3ab51fa25d215e4bfeaeb556bca16f68ceea122f
Oleg Cherkunov <oleg@generalarcade.com>
2025-01-02 13:15:45 +0800

    [Win] Change OpenGL debug message func

--- Engine/Fuego/Windows/OpenGL/SwapchainOpenGL.cpp
+++ Engine/Fuego/Windows/OpenGL/SwapchainOpenGL.cpp
@@ -27,7 +27,10 @@ SwapchainOpenGL::SwapchainOpenGL(const Surface& surface)
     FU_CORE_INFO("  Renderer: {0}", reinterpret_cast<const char*>(glGetString(GL_RENDERER)));
     // TODO: if debuf then enable OpenGL debug callback:
     glEnable(GL_DEBUG_OUTPUT);
+    glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS);
     glDebugMessageCallback(SwapchainOpenGL::OpenGLDebugCallbackFunc, nullptr);
+    glDebugMessageControl(GL_DONT_CARE, GL_DONT_CARE, GL_DONT_CARE, 0, nullptr, GL_TRUE);
+
     glEnable(GL_DEPTH_TEST);
 }
 
@@ -53,17 +56,80 @@ Surface& SwapchainOpenGL::GetScreenTexture()
 void APIENTRY SwapchainOpenGL::OpenGLDebugCallbackFunc(GLenum source, GLenum type, GLuint id, GLenum severity, GLsizei length, const GLchar* message,
                                                        const void* userParam)
 {
-    GLenum error = glGetError();
-    if (error == GL_NO_ERROR)
+    if (id == 131169 || id == 131185 || id == 131218 || id == 131204)
+        return;
+    FU_CORE_ERROR("---- OpenGL Debug Message ----");
+    switch (source)
     {
-        FU_CORE_INFO("[OpenGL] \nMessage: {0}\nSource: {1}\nType: {2}\nID: {3}\nSeverity: {4}",
-            (const char*)message, source, type, id, severity);
+    case GL_DEBUG_SOURCE_API:
+        FU_CORE_ERROR("Source: API");
+        break;
+    case GL_DEBUG_SOURCE_WINDOW_SYSTEM:
+        FU_CORE_ERROR("Source: Window System");
+        break;
+    case GL_DEBUG_SOURCE_SHADER_COMPILER:
+        FU_CORE_ERROR("Source: Shader Compiler");
+        break;
+    case GL_DEBUG_SOURCE_THIRD_PARTY:
+        FU_CORE_ERROR("Source: Third Party");
+        break;
+    case GL_DEBUG_SOURCE_APPLICATION:
+        FU_CORE_ERROR("Source: Application");
+        break;
+    case GL_DEBUG_SOURCE_OTHER:
+        FU_CORE_ERROR("Source: Other");
+        break;
     }
-    else
+
+    switch (type)
     {
-        FU_CORE_ERROR("[OpenGL] \nMessage: {0}\nSource: {1}\nType: {2}\nID: {3}\nSeverity: {4}",
-            (const char*)message, source, type, id, severity);
+    case GL_DEBUG_TYPE_ERROR:
+        FU_CORE_ERROR("Type: Error");
+        break;
+    case GL_DEBUG_TYPE_DEPRECATED_BEHAVIOR:
+        FU_CORE_ERROR("Type: Deprecated Behaviour");
+        break;
+    case GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR:
+        FU_CORE_ERROR("Type: Undefined Behaviour");
+        break;
+    case GL_DEBUG_TYPE_PORTABILITY:
+        FU_CORE_ERROR("Type: Portability");
+        break;
+    case GL_DEBUG_TYPE_PERFORMANCE:
+        FU_CORE_ERROR("Type: Performance");
+        break;
+    case GL_DEBUG_TYPE_MARKER:
+        FU_CORE_ERROR("Type: Marker");
+        break;
+    case GL_DEBUG_TYPE_PUSH_GROUP:
+        FU_CORE_ERROR("Type: Push Group");
+        break;
+    case GL_DEBUG_TYPE_POP_GROUP:
+        FU_CORE_ERROR("Type: Pop Group");
+        break;
+    case GL_DEBUG_TYPE_OTHER:
+        FU_CORE_ERROR("Type: Other");
+        break;
     }
+
+    switch (severity)
+    {
+    case GL_DEBUG_SEVERITY_HIGH:
+        FU_CORE_ERROR("Severity: High");
+        break;
+    case GL_DEBUG_SEVERITY_MEDIUM:
+        FU_CORE_ERROR("Severity: Medium");
+        break;
+    case GL_DEBUG_SEVERITY_LOW:
+        FU_CORE_ERROR("Severity: Low");
+        break;
+    case GL_DEBUG_SEVERITY_NOTIFICATION:
+        FU_CORE_ERROR("Severity: Notification");
+        break;
+    }
+   
+        FU_CORE_ERROR("Message: {0}, Source: {1}, Type: {2}, ID: {3}, Severity: {4}\n",
+            (const char*)message, source, type, id, severity);
 }
 
 void SwapchainOpenGL::ShowWireFrame(bool show)
