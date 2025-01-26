ab81a0afc4bf63e6f312146cb212c1d2540c16af
Oleg Cherkunov <oleg@generalarcade.com>
2025-01-26 19:53:12 +0800

    Add material to shader and format shaders

--- Sandbox/Resources/Windows/Shaders/ps_triangle.glsl
+++ Sandbox/Resources/Windows/Shaders/ps_triangle.glsl
@@ -1,9 +1,15 @@
 #version 330 core
 
-in vec2 text_coords;
-
+struct Material
+{
+    sampler2D albedo_text;
+}
+// Uniforms:
+uniform Material material;
 uniform sampler2D gSampler;
-
+// In:
+in vec2 text_coords;
+// Out:
 out vec4 FragColor;
 
 void main()
--- Sandbox/Resources/Windows/Shaders/vs_shader.glsl
+++ Sandbox/Resources/Windows/Shaders/vs_shader.glsl
@@ -1,13 +1,15 @@
 #version 330 core
-
+// Layout:
 layout(location = 0) in vec3 aPos;
 layout(location = 1) in vec2 aTextCoord;
 layout(location = 2) in vec3 aNormal;  
-
+// Uniforms:
 uniform mat4 model;
 uniform mat4 view;
 uniform mat4 projection;
+// In:
 
+// Out:
 out vec2 text_coords;
 
 void main()
