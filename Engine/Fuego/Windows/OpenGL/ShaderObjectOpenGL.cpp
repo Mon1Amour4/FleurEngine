3423621172f9f7e7f30c9d1948b07b4fc830d953
Oleg Cherkunov <oleg@generalarcade.com>
2025-01-27 13:31:02 +0800

    Add ShaderObject OpenGL

---
+++ Engine/Fuego/Windows/OpenGL/ShaderObjectOpenGL.cpp
@@ -0,0 +1,46 @@
+#include "ShaderObjectOpenGL.h"
+
+namespace Fuego::Renderer
+{
+
+ShaderObject* ShaderObject::CreateShaderObject(Shader& vs, Shader& px)
+{
+    return new ShaderObjectOpenGL(vs, px);
+}
+
+ShaderObjectOpenGL::ShaderObjectOpenGL(Shader& vs, Shader& px)
+    : vertex_shader(nullptr)
+    , pixel_shader(nullptr)
+    , program(glCreateProgram())
+{
+    auto vs_gl = static_cast<ShaderOpenGL*>(&vs);
+    auto px_gl = static_cast<ShaderOpenGL*>(&px);
+    vertex_shader = vs_gl;
+    pixel_shader = px_gl;
+
+    glAttachShader(program, vs_gl->GetID());
+    glAttachShader(program, px_gl->GetID());
+
+    glLinkProgram(program);
+    GLint success;
+    glGetProgramiv(program, GL_LINK_STATUS, &success);
+    if (!success)
+    {
+        char infoLog[512];
+        glGetProgramInfoLog(program, 512, nullptr, infoLog);
+        FU_CORE_ERROR("[ShaderObject] program linking error: ", infoLog);
+    }
+    vs_gl->BindToShaderObject(*this);
+    px_gl->BindToShaderObject(*this);
+
+    glUseProgram(program);
+}
+
+ShaderObjectOpenGL::~ShaderObjectOpenGL()
+{
+    glDeleteProgram(program);
+    glDeleteShader(vertex_shader->GetID());
+    glDeleteShader(pixel_shader->GetID());
+}
+
+}  // namespace Fuego::Renderer
---
+++ Engine/Fuego/Windows/OpenGL/ShaderObjectOpenGL.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include "ShaderObject.h"
+#include "ShaderOpenGL.h"
+
+namespace Fuego::Renderer
+{
+class ShaderObjectOpenGL : public ShaderObject
+{
+public:
+    virtual ~ShaderObjectOpenGL() override;
+
+    inline virtual Shader* GetPixelShader() override
+    {
+        return pixel_shader;
+    }
+    inline virtual Shader* GetVertexShader() override
+    {
+        return vertex_shader;
+    }
+    inline virtual uint16_t GetObjectID()
+    {
+        return program;
+    }
+
+private:
+    uint16_t program;
+    ShaderOpenGL* vertex_shader;
+    ShaderOpenGL* pixel_shader;
+
+    friend class ShaderObject;
+    ShaderObjectOpenGL(Shader& vs, Shader& px);
+};
+}  // namespace Fuego::Renderer
