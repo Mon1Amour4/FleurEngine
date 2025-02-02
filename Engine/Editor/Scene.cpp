5bcd48db8ffb00051184be97b35acbaf1d97891e
Oleg Cherkunov <mon1amour4@gmail.com>
2025-02-02 18:44:07 +0800

    Add BaseSceneObject, SceneObject, SceneFolder, ModelObject

--- Engine/Editor/Scene.cpp
+++ Engine/Editor/Scene.cpp
@@ -9,9 +9,15 @@ uint32_t TreeNode::GetID()
     return ++s_id;
 }
 
-TreeNode::TreeNode()
-    : id(GetID())
+TreeNode::TreeNode(BaseSceneObject* obj, uint16_t node_level)
+    : object(obj)
+    , node_level(node_level)
+    , id(GetID())
 {
+}
+TreeNode::~TreeNode()
+{
+    delete object;
 }
 bool TreeNode::operator==(const TreeNode& other) const
 {
@@ -35,6 +41,75 @@ void TreeNode::RemoveChild(TreeNode& child)
         }
     }
 }
+uint16_t TreeNode::GetNodeLevel() const
+{
+    return node_level;
+}
+void TreeNode::PrintNode() const
+{
+    FU_CORE_TRACE("Node name: {0}, level: {1}", object->GetName(), node_level);
+    for (std::list<TreeNode>::const_iterator it = children.begin(); it != children.end(); ++it)
+    {
+        FU_CORE_TRACE("Child: {0}", it->object->GetName());
+    }
+}
+Root::Root()
+    : TreeNode(new SceneFolder("Root Folder"), 0)
+{
+}
+Node::Node(const std::string& node_name, BaseSceneObject* obj, TreeNode* parent)
+    : TreeNode(obj, parent->GetNodeLevel() + 1)
+    , parent(parent)
+{
+}
+void Node::PrintNode() const
+{
+    TreeNode::PrintNode();
+    Node* par = dynamic_cast<Node*>(parent);
+    if (par)
+    {
+        FU_CORE_TRACE("Parent Node: {0}", par->GetSceneObject()->GetName());
+    }
+    else
+    {
+        FU_CORE_TRACE("Parent Node is Root node");
+    }
+}
+BaseSceneObject::BaseSceneObject(const std::string& name, bool enabled)
+    : name(name)
+    , enabled(enabled)
+{
+    FU_CORE_INFO("BaseSceneObject: {0} ctor", this->name);
+}
+SceneFolder::SceneFolder(const std::string& folder_name)
+    : BaseSceneObject(folder_name, true)
+{
+    FU_CORE_INFO("SceneFolder: {0} ctor", ((BaseSceneObject*)this)->GetName());
+}
+SceneObject::SceneObject(const std::string& name, glm::vec3 pos, glm::vec3 rot)
+    : BaseSceneObject(name, true)
+    , position(pos)
+    , rotation(rot)
+{
+    FU_CORE_INFO("SceneObject: {0} ctor", ((BaseSceneObject*)this)->GetName());
+}
+ModelObject::ModelObject(const std::string& name, glm::vec3 pos, glm::vec3 rot, const Material* mat)
+    : SceneObject(name, pos, rot)
+    , material(mat)
+{
+    FU_CORE_INFO("ModelObject: {0} ctor", ((BaseSceneObject*)this)->GetName());
+}
 
+
+Scene::Scene(const std::string& scene_name, Root* root)
+    : scene_name(scene_name)
+    , root(root)
+{
+}
+
+Scene::~Scene()
+{
+    delete root;
+}
 
 }  // namespace Fuego::Editor
--- Engine/Editor/Scene.h
+++ Engine/Editor/Scene.h
@@ -9,61 +9,112 @@
 
 namespace Fuego::Editor
 {
+class Material;
+class Root;
+
 class Scene
 {
 public:
+    Scene(const std::string& scene_name, Root* root);
+    ~Scene();
+
+    inline const Root* GetRootNode() const
+    {
+        return root;
+    }
+
 private:
+    Root* root;
+    std::string scene_name;
     uint16_t objects_amount;
 };
-
-class SceneObject
+class BaseSceneObject
 {
 public:
-    virtual ~SceneObject();
+    BaseSceneObject(const std::string& name, bool enabled);
+    virtual ~BaseSceneObject() = default;
+
+    inline const std::string& GetName() const
+    {
+        return name;
+    }
 
 private:
     std::string name;
-    glm::vec3 position;
-    glm::vec3 rotation;
     bool enabled;
 };
+class SceneFolder : public BaseSceneObject
+{
+public:
+    SceneFolder(const std::string& folder_name);
+};
+class SceneObject : public BaseSceneObject
+{
+public:
+    SceneObject(const std::string& name, glm::vec3 pos = glm::vec3(0.0f), glm::vec3 rot = glm::vec3(0.0f));
+
+private:
+    glm::vec3 position;
+    glm::vec3 rotation;
+};
+class ModelObject : public SceneObject
+{
+public:
+    ModelObject(const std::string& name, glm::vec3 pos, glm::vec3 rot, const Material* mat);
+
+private:
+    const Material* material;
+};
+
 
 class TreeNode
 {
 public:
-    TreeNode();
+    TreeNode(BaseSceneObject* obj, uint16_t node_level);
+    virtual ~TreeNode();
+
     bool operator==(const TreeNode& other) const;
-    virtual ~TreeNode() = default;
 
     virtual void AddChildFront(TreeNode&& child);
     virtual void AddChildBack(TreeNode&& child);
     virtual void RemoveChild(TreeNode& child);
 
+    uint16_t GetNodeLevel() const;
+    inline const BaseSceneObject* GetSceneObject() const
+    {
+        return object;
+    }
+
+    virtual void PrintNode() const;
 
 protected:
-    std::list<TreeNode> children;
-    uint32_t id;
-    static uint32_t s_id;
+    static uint32_t GetID();
+    uint16_t node_level;
 
 private:
-    static uint32_t GetID();
-};
+    BaseSceneObject* object;
 
-class Root : public TreeNode
+    std::list<TreeNode> children;
+
+    uint32_t id;
+    static uint32_t s_id;
+};
+class Root final : public TreeNode
 {
 public:
     Root();
-
-protected:
-private:
 };
-
-class Node : public TreeNode
+class Node final : public TreeNode
 {
 public:
-    Node();
+    Node(const std::string& node_name, BaseSceneObject* obj, TreeNode* parent);
 
-protected:
+    inline const TreeNode* GetParent() const
+    {
+        return parent;
+    }
+    virtual void PrintNode() const override;
+
 private:
     TreeNode* parent;
 };
