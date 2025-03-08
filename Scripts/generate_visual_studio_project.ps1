88560d85f185eb1a94f85e6c1e5b61a6f5b7aab3
Oleg Cherkunov <mon1amour4@gmail.com>
2025-03-08 18:29:07 +0800

    Add Tests executable and link corelibtests as static lib

--- Scripts/generate_visual_studio_project.ps1
+++ Scripts/generate_visual_studio_project.ps1
@@ -1,6 +1,6 @@
 $currentDir = Get-Location
 Set-Location -Path $PSScriptRoot
 $platform = "windows"
-python .\generate_project.py $platform
+python .\generate_project.py $platform true
 Set-Location -Path $currentDir
 Write-Host "Original directory: $currentDir"
