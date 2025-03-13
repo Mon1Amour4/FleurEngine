c35402fdbc5a8697df6aabe3022c9dca652c8e3d
Oleg Cherkunov <mon1amour4@gmail.com>
2025-03-13 15:45:24 +0800

    Add resources macOS paths

--- Engine/Fuego/MacOS/FileSystemPathsMacOS.h
+++ Engine/Fuego/MacOS/FileSystemPathsMacOS.h
@@ -1,6 +1,7 @@
 #pragma once
 
-static std::string resource = "STAB";
-static std::string shaders = "STAB";
-static std::string images = "STAB";
-static std::string models = "STAB";
+static std::string resource = "../../../Sandbox/Resources/";
+static std::string shaders = "MacOS/Shaders/";
+static std::string images = "MacOS/Images/";
+static std::string models = "MacOS/Models/";
+static std::string scenes = "MacOS/Scenes/";
