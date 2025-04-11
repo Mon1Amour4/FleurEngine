962679de4137188be976a60bdd9f560732b80742
Oleg Cherkunov <mon1amour4@gmail.com>
2025-04-11 12:29:04 +0800

    Fill Model class using assimp data

--- Engine/Fuego/Renderer/Model.cpp
+++ Engine/Fuego/Renderer/Model.cpp
@@ -1,4 +1,5 @@
 #include "Model.h"
+#include "Renderer.h"
 
 #include <filesystem>
 
@@ -11,11 +12,14 @@ Fuego::Renderer::Model::Model(const aiScene* scene)
     : name(scene->mRootNode->mName.C_Str())
     , mesh_count(scene->mNumMeshes)
     , vertex_count(0)
+    , indices_count(0)
 {
     meshes.reserve(scene->mNumMeshes);
     for (size_t i = 0; i < scene->mNumMeshes; i++)
     {
-        meshes.emplace_back(std::make_unique<Fuego::Renderer::Model::Mesh>(scene->mMeshes[i], scene->mMaterials));
+        meshes.emplace_back(std::make_unique<Fuego::Renderer::Model::Mesh>(scene->mMeshes[i], scene->mMaterials, i, vertices, indices));
+        vertex_count += scene->mMeshes[i]->mNumVertices;
+        indices_count += meshes.back().get()->GetIndicesCount();
     }
 }
 
@@ -44,10 +48,41 @@ Fuego::Renderer::Model& Fuego::Renderer::Model::operator=(Model&& other) noexcep
     return *this;
 }
 
-Fuego::Renderer::Model::Mesh::Mesh(aiMesh* mesh, aiMaterial** materials)
+Fuego::Renderer::Model::Mesh::Mesh(aiMesh* mesh, aiMaterial** materials, uint16_t mesh_index, std::vector<Fuego::Renderer::VertexData>& vertices,
+                                   std::vector<uint32_t>& indices)
     : mesh_name(mesh->mName.C_Str())
     , material(materials[mesh->mMaterialIndex]->GetName().C_Str())
     , vertex_count(mesh->mNumVertices)
+    , start_index(indices.size() - 1)
+    , end_index(0)
+    , indices_count(0)
 {
-    
+    vertices.reserve(vertex_count);
+    for (size_t i = 0; i < vertex_count; i++)
+    {
+        vertices.emplace_back(glm::vec3(mesh->mVertices[i].x, mesh->mVertices[i].y, mesh->mVertices[i].z));
+        VertexData& vertex = vertices.back();
+        if (mesh->HasNormals())
+        {
+            vertex.normal.x = mesh->mNormals[i].x;
+            vertex.normal.y = mesh->mNormals[i].y;
+            vertex.normal.z = mesh->mNormals[i].z;
+        }
+        // TODO: mesh may contain up to 8 different texture, 
+        // use first one for now, later increase textures up to 8?
+        if (mesh->HasTextureCoords(mesh_index))
+        {
+            vertex.textcoord.x = mesh->mTextureCoords[0]->x;
+            vertex.textcoord.y = mesh->mTextureCoords[0]->y;
+        }
+    }
+    for (size_t i = 0; i < mesh->mNumFaces; i++)
+    {
+        aiFace face = mesh->mFaces[i];
+        for (size_t j = 0; j < face.mNumIndices; j++)
+            indices.push_back(face.mIndices[j]);
+        indices_count += face.mNumIndices;
+    }
+    end_index = indices.size() - 1;
+
 }
--- Engine/Fuego/Renderer/Model.h
+++ Engine/Fuego/Renderer/Model.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#include "Renderer.h"
+//#include "Renderer.h"
 #include "glm/ext.hpp"
 #include "glm/glm.hpp"
 
@@ -8,8 +8,11 @@ struct aiScene;
 struct aiMesh;
 struct aiMaterial;
 
+
 namespace Fuego::Renderer
 {
+struct VertexData;
+
 class FUEGO_API Model
 {
 public:
@@ -31,28 +34,53 @@ public:
     {
         return vertex_count;
     }
+    inline uint16_t GetIndicesCount() const
+    {
+        return indices_count;
+    }
+
+    inline const float* GetVerticesData() const
+    {
+        return (float*)vertices.data();
+    }
+    inline const uint32_t* GetIndicesData() const
+    {
+        return (uint32_t*)indices.data();
+    }
 
 private:
     std::string name;
     uint16_t mesh_count;
     uint32_t vertex_count;
+    uint32_t indices_count;
+    std::vector<Fuego::Renderer::VertexData> vertices;
+    std::vector<uint32_t> indices;
 
 public:
     class FUEGO_API Mesh
     {
     public:
-        Mesh(aiMesh* mesh, aiMaterial** materials);
+        Mesh(aiMesh* mesh, aiMaterial** materials, uint16_t mesh_index, std::vector<Fuego::Renderer::VertexData>& vertices, std::vector<uint32_t>& indices);
         ~Mesh() = default;
 
-        inline unsigned short int GetVertexCount() const
+        inline uint16_t GetVertexCount() const
         {
             return vertex_count;
+        }
+        inline uint16_t GetIndicesCount() const
+        {
+            return indices_count;
         }
 
     private:
         std::string mesh_name;
         std::string material;
-        unsigned short int vertex_count;
+
+        uint32_t start_index;
+        uint32_t end_index;
+
+        uint16_t vertex_count;
+        uint16_t indices_count;
     };
 
 private:
