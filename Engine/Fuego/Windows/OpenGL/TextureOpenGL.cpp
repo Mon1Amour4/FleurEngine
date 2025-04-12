f223fb8d2b279d08305bf5ee1a4d53f0364fdc29
Oleg Cherkunov <mon1amour4@gmail.com>
2025-04-13 01:42:47 +0800

    Generate Mipmaps for textures

--- Engine/Fuego/Windows/OpenGL/TextureOpenGL.cpp
+++ Engine/Fuego/Windows/OpenGL/TextureOpenGL.cpp
@@ -13,11 +13,14 @@ TextureOpenGL::TextureOpenGL(unsigned char* buffer, int width, int heigth)
     glBindTexture(GL_TEXTURE_2D, texture_id);
     glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, heigth, 0, GL_RGB, GL_UNSIGNED_BYTE, buffer);
 
+    glGenerateMipmap(GL_TEXTURE_2D);
+
     // Configuration of minification/Magnification
-    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
     glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
     glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
     glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+
     glBindTexture(GL_TEXTURE_2D, 0);
 }
 
