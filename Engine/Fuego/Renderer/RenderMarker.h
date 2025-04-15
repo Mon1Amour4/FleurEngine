070ce6573a77435b3909ec0f48525eb1e43d3050
Oleg Cherkunov <mon1amour4@gmail.com>
2025-04-15 20:31:01 +0800

    Add basic API to RenderMarker base class

--- Engine/Fuego/Renderer/RenderMarker.h
+++ Engine/Fuego/Renderer/RenderMarker.h
@@ -5,8 +5,32 @@ namespace Fuego::Renderer::Debug
 class RenderMarker
 {
 public:
-    ~RenderMarker() = default;
+    virtual ~RenderMarker() = 0;
 
-    // TODO
+
+private:
+    RenderMarker();
+};
+
+struct DebugGroup
+{
+public:
+    DebugGroup();
+
+private:
+    virtual ~DebugGroup() = 0;
+
+    virtual void Push(uint32_t id, uint16_t object_name, uint16_t label_length, const char* label) = 0;
+    virtual void Pop() = 0;
+};
+
+struct ObjectLabel
+{
+public:
+    virtual ~ObjectLabel() = 0;
+
+    virtual void SetLabel(uint32_t id, uint16_t object_name, uint16_t label_length, const char* label) = 0;
+
+private:
 };
 }  // namespace Fuego::Renderer::Debug
