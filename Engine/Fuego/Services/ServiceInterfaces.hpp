3accf45c7b6c89461266eaa5739fb4d20c9721e5
Oleg Cherkunov <mon1amour4@gmail.com>
2025-05-03 03:38:45 +0800

    Change template argument constraints cause it didn't really work

--- Engine/Fuego/Services/ServiceInterfaces.hpp
+++ Engine/Fuego/Services/ServiceInterfaces.hpp
@@ -15,18 +15,12 @@ struct IRendererService
     virtual void DrawModel(const Fuego::Renderer::Model* model, glm::mat4 model_pos) = 0;
     virtual void ChangeViewport(float x, float y, float w, float h) = 0;
     virtual std::unique_ptr<Fuego::Renderer::Texture> CreateTexture(unsigned char* buffer, int width, int height) const = 0;
-
-protected:
-    static constexpr bool is_renderer = true;
 };
 
 struct IFileSystemService
 {
     virtual void FUCreateFile(const std::string& file_name, std::string_view folder) const = 0;
     virtual void WriteToFile(std::string_view file_name, const char* buffer) = 0;
-
-protected:
-    static constexpr bool is_file_system = true;
 };
 
 struct IEngineSubSystem
