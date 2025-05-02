bc6a387abc2c3ee0ec16b5d2e24fc96fea38a5d1
Oleg Cherkunov <mon1amour4@gmail.com>
2025-05-03 02:13:01 +0800

    Add Service interfaces, ServiceLocator

---
+++ Engine/Fuego/Services/ServiceLocator.h
@@ -0,0 +1,77 @@
+#pragma once
+#include <typeindex>
+// #include <typeinfo>
+#include <unordered_map>
+#include <variant>
+
+#include "FileSystem/FileSystem.h"
+#include "Renderer.h"
+#include "Services/ServiceInterfaces.hpp"
+#include "singleton.hpp"
+
+namespace Fuego
+{
+
+#pragma region Templates
+using service_variant = std::variant<std::unique_ptr<Fuego::Renderer::Renderer>, std::unique_ptr<Fuego::FS::FileSystem>>;
+
+
+template <class T>
+concept is_renderer_service = requires(T t) { T::is_renderer == true; };
+
+template <class T>
+concept is_file_system_service = requires(T t) { T::is_file_system == true; };
+
+template <class T>
+struct CheckRenderer
+{
+    static constexpr bool value = requires { is_renderer_service<T>; };
+};
+
+template <class T>
+struct CheckFileSystem
+{
+    static constexpr bool value = requires { is_file_system_service<T>; };
+};
+#pragma endregion
+
+class ServiceLocator : public singleton<ServiceLocator>
+{
+    friend class singleton<ServiceLocator>;
+
+public:
+    template <class T, template <class> class U>
+    std::optional<T*> AddService()
+    {
+        if constexpr (U<T>::value == true)
+
+        {
+            auto ptr = std::make_unique<T>();
+            service_variant variant = std::move(ptr);
+            auto [it, inserted] = services.try_emplace(std::type_index(typeid(T)), std::move(variant));
+            if (auto pval = std::get_if<std::unique_ptr<T>>(&it->second))
+            {
+                return pval->get();
+            }
+        }
+        return std::nullopt;
+    };
+
+    template <class T>
+    T* GetService()
+    {
+        auto it = services.find(std::type_index(typeid(T)));
+        if (it == services.end())
+            return nullptr;
+
+        if (auto ptr = std::get_if<std::unique_ptr<T>>(&it->second))
+            return ptr->get();
+
+        return nullptr;
+    }
+
+private:
+    ServiceLocator() = default;
+    std::unordered_map<std::type_index, service_variant> services;
+};
+}  // namespace Fuego
