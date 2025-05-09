175c4e10acb430077526bfcb2cffa485f5b06713
Oleg Cherkunov <mon1amour4@gmail.com>
2025-05-09 19:13:20 +0800

    Change Services concept, it needs Init() and Shutdown() implemented interface

--- Engine/Fuego/Services/ServiceInterfaces.hpp
+++ Engine/Fuego/Services/ServiceInterfaces.hpp
@@ -1,6 +1,16 @@
 #pragma once
 #include <glm/ext/matrix_float4x4.hpp>
 
+#pragma region Concepts
+
+template <typename Derived>
+concept is_service_interface = requires(Derived t) {
+    { t.Init() } -> std::same_as<void>;
+    { t.Shutdown() } -> std::same_as<void>;
+};
+
+#pragma endregion
+
 namespace Fuego::Graphics
 {
 class Model;
@@ -10,7 +20,6 @@ class Renderer;
 
 namespace Fuego
 {
-
 
 struct IRendererService
 {
@@ -23,12 +32,6 @@ struct IFileSystemService
 {
     virtual void FUCreateFile(const std::string& file_name, std::string_view folder) const = 0;
     virtual void WriteToFile(std::string_view file_name, const char* buffer) = 0;
-};
-
-template <typename Derived>
-concept is_service_interface = requires(Derived t) {
-    { t.Init() } -> std::same_as<void>;
-    { t.Shutdown() } -> std::same_as<void>;
 };
 
 struct IUpdatable
@@ -45,10 +48,29 @@ struct IInitializable
 };
 
 template <class Derived, class... Interfaces>
-struct Service : public IInitializable
+struct Service : public IInitializable, public Interfaces...
 {
+    void Init()
+    {
+        if (is_initialized)
+            return;
+        Derived* derived = static_cast<Derived*>(this);
+        derived->OnInit();
+        is_initialized = true;
+    };
+
+    void Shutdown()
+    {
+        if (shutdown)
+            return;
+        Derived* derived = static_cast<Derived*>(this);
+        derived->OnShutdown();
+        shutdown = true;
+    }
+
 protected:
     inline static bool is_initialized = false;
+    inline static bool shutdown = false;
 };
 
 }  // namespace Fuego
