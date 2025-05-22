31d331396880a93072acd69cd0d7a244b48640ae
Oleg Cherkunov <mon1amour4@gmail.com>
2025-05-23 03:55:25 +0800

    Use GL_REPEAT textures parameter

--- Engine/Fuego/Windows/OpenGL/TextureOpenGL.cpp
+++ Engine/Fuego/Windows/OpenGL/TextureOpenGL.cpp
@@ -23,8 +23,8 @@ TextureOpenGL::TextureOpenGL(std::string_view name, TextureFormat format, unsign
     // Configuration of minification/Magnification
     glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
     glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
 
     glBindTexture(GL_TEXTURE_2D, 0);
 }
