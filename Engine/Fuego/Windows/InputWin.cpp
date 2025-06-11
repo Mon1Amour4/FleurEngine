eb6940eb87056a3241f177d75521dda7c108b369
Oleg Cherkunov <mon1amour4@gmail.com>
2025-06-11 23:00:02 +0800

    Use static_cast instead of reinterpret_cast

--- Engine/Fuego/Windows/InputWin.cpp
+++ Engine/Fuego/Windows/InputWin.cpp
@@ -14,19 +14,21 @@ InputWin& singleton<InputWin>::instance()
 
 bool InputWin::IsKeyPressedImpl(KeyCode keyCode) const
 {
-    const WindowWin& window = reinterpret_cast<const WindowWin&>(Application::instance().GetWindow());
+    const WindowWin& window = static_cast<const WindowWin&>(Application::instance().GetWindow());
     Input::KeyState state = window.GetKeyState(keyCode);
     return state == Input::KEY_PRESSED || state == Input::KEY_REPEAT;
 }
+
 bool InputWin::IsMouseButtonPressedImpl(uint16_t mouseCode)
 {
-    const WindowWin& window = reinterpret_cast<const WindowWin&>(Application::instance().GetWindow());
+    const WindowWin& window = static_cast<const WindowWin&>(Application::instance().GetWindow());
     Input::MouseState state = window.GetMouseState(mouseCode);
     return state == Input::MOUSE_LPRESSED || state == Input::MOUSE_RPRESSED;
 }
+
 std::pair<float, float> InputWin::GetMousePositionImpl() const
 {
-    const WindowWin& window = reinterpret_cast<const WindowWin&>(Application::instance().GetWindow());
+    const WindowWin& window = static_cast<const WindowWin&>(Application::instance().GetWindow());
     float xPos, yPos;
     window.GetMousePos(xPos, yPos);
     return {xPos, yPos};
@@ -46,6 +48,7 @@ float InputWin::GetMouseXImpl() const
     auto [x, y] = GetMousePositionImpl();
     return x;
 }
+
 float InputWin::GetMouseYImpl() const
 {
     auto [x, y] = GetMousePositionImpl();
@@ -54,7 +57,7 @@ float InputWin::GetMouseYImpl() const
 
 glm::vec2 InputWin::GetMouseDirImpl() const
 {
-    const WindowWin& window = reinterpret_cast<const WindowWin&>(Application::instance().GetWindow());
+    const WindowWin& window = static_cast<const WindowWin&>(Application::instance().GetWindow());
     return window.GetMouseDir();
 }
 
