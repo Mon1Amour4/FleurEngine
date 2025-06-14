c7b0767071d74c9b51e230207d8b879257c396cc
Oleg Cherkunov <mon1amour4@gmail.com>
2025-06-14 23:12:00 +0800

    Define abstract CommandBuffer interface for graphics backend abstraction
    Adds virtual methods for common rendering tasks such as buffer binding, shader dispatch,
    data upload, and draw call submission. Serves as the backend-agnostic layer for future
    OpenGL, Vulkan, or DirectX implementations.

--- Engine/Fuego/Renderer/CommandBuffer.h
+++ Engine/Fuego/Renderer/CommandBuffer.h
@@ -1,4 +1,7 @@
 #pragma once
+#include <span>
+
+#include "Buffer.h"
 
 namespace Fuego::Graphics
 {
@@ -10,10 +13,11 @@ class ShaderObject;
 class Texture;
 struct VertexLayout;
 class Device;
+enum RenderStage;
 
 class CommandBuffer
 {
-public:
+   public:
     enum ObjectLabel
     {
         LABEL_BUFFER = 0,
@@ -28,10 +32,13 @@ public:
     virtual void EndRecording() = 0;
     virtual void Submit() = 0;
     virtual void BindRenderTarget(const Surface& texture) = 0;
-    virtual void BindShaderObject(const ShaderObject& obj) = 0;
+    virtual void BindShaderObject(std::shared_ptr<Fuego::Graphics::ShaderObject> shader) = 0;
     virtual void BindDescriptorSet(const DescriptorBuffer& descriptorSet, int setIndex) = 0;
-    virtual void BindVertexBuffer(const Buffer& vertexBuffer, VertexLayout layout) = 0;
-    virtual void BindIndexBuffer(const uint32_t indices[], uint32_t size) = 0;
+
+    virtual void BindVertexBuffer(std::unique_ptr<Fuego::Graphics::Buffer> vertexBuffer, VertexLayout layout) = 0;
+    virtual void UpdateSubData(const void* data, size_t size_bytes, size_t offset) = 0;
+
+    virtual void BindIndexBuffer(RenderStage stage, const uint32_t indices[], uint32_t size_bytes) = 0;
     virtual void BindTexture(Texture* texture) = 0;
     virtual void Draw(uint32_t vertexCount) = 0;
     virtual void IndexedDraw(uint32_t vertexCount, const void* indices_ptr_offset) = 0;
@@ -41,14 +48,15 @@ public:
     virtual void PopDebugGroup() = 0;
     virtual void SetLabel(ObjectLabel id, uint32_t name, const char* message) = 0;
 
-protected:
-    CommandBuffer()
-        : push_debug_group_commands(0)
-    {
-    }
+    virtual Fuego::Graphics::ShaderObject* ShaderObject() const { return shader_object.get(); }
 
-protected:
+   protected:
+    CommandBuffer() : push_debug_group_commands(0) {};
+
     uint16_t push_debug_group_commands;
+
+    std::shared_ptr<Fuego::Graphics::ShaderObject> shader_object;
+    std::unique_ptr<Fuego::Graphics::Buffer> buffer;
 };
 
 }  // namespace Fuego::Graphics
