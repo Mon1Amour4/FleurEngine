992fec909373a2605d17774e484dd3ccd7b09e00
Oleg Cherkunov <Mon1Amour4@gmail.com>
2025-07-05 20:30:15 +0800

    Refactor OpenGL Cubemap

--- Engine/Fuego/Windows/OpenGL/CubemapOpenGL.cpp
+++ Engine/Fuego/Windows/OpenGL/CubemapOpenGL.cpp
@@ -5,6 +5,7 @@
 #include <glm/geometric.hpp>
 #include <glm/trigonometric.hpp>
 
+#include "../../../../CoreLib/Bitmap.hpp"
 #include "glad/wgl.h"
 
 Fuego::Graphics::CubemapOpenGL::CubemapOpenGL(const Image2D* img_1, const Image2D* img_2, const Image2D* img_3, const Image2D* img_4, const Image2D* img_5,
@@ -49,11 +50,18 @@ Fuego::Graphics::CubemapOpenGL::CubemapOpenGL(const Image2D* equirectangular)
 {
     FU_CORE_ASSERT(equirectangular->Width() / equirectangular->Height() == 2.0f, "");
 
-    float face_size = equirectangular->Width() / 4.f;
+    Fuego::Bitmap<BitmapFormat_UnsignedByte> equirectangular_bitmap(equirectangular);
+
+    uint32_t face_size = equirectangular->Width() / 4.f;
     constexpr float pi = glm::pi<float>();
+
+    glCreateTextures(GL_TEXTURE_CUBE_MAP, 1, &id);
+    glTextureStorage2D(id, 1, GL_RGB8, face_size, face_size);
 
     for (size_t face = 0; face < 6; face++)
     {
+        Fuego::Bitmap<BitmapFormat_UnsignedByte> bitmap(face_size, face_size, equirectangular->Channels());
+
         for (size_t coord_u = 0; coord_u < face_size; coord_u++)
         {
             for (size_t coord_v = 0; coord_v < face_size; coord_v++)
@@ -64,22 +72,22 @@ Fuego::Graphics::CubemapOpenGL::CubemapOpenGL(const Image2D* equirectangular)
                 glm::vec3 direction;
                 switch (face)
                 {
-                case GL_TEXTURE_CUBE_MAP_POSITIVE_X:
+                case 0:
                     direction = glm::vec3(1.f, normalized_v, -normalized_u);
                     break;
-                case GL_TEXTURE_CUBE_MAP_NEGATIVE_X:
+                case 1:
                     direction = glm::vec3(-1.f, normalized_v, normalized_u);
                     break;
-                case GL_TEXTURE_CUBE_MAP_POSITIVE_Y:
+                case 2:
                     direction = glm::vec3(normalized_u, 1.f, normalized_v);
                     break;
-                case GL_TEXTURE_CUBE_MAP_NEGATIVE_Y:
+                case 3:
                     direction = glm::vec3(normalized_u, -1.f, -normalized_v);
                     break;
-                case GL_TEXTURE_CUBE_MAP_POSITIVE_Z:
+                case 4:
                     direction = glm::vec3(normalized_u, normalized_v, 1.f);
                     break;
-                case GL_TEXTURE_CUBE_MAP_NEGATIVE_Z:
+                case 5:
                     direction = glm::vec3(-normalized_u, normalized_v, -1.f);
                     break;
                 }
@@ -92,7 +100,27 @@ Fuego::Graphics::CubemapOpenGL::CubemapOpenGL(const Image2D* equirectangular)
 
                 float u = static_cast<float>((phi + pi) / (2.f * pi));
                 float v = static_cast<float>((pi / 2.f - theta) / pi);
+
+                glm::vec4 color = equirectangular_bitmap.GetPixel(u, v);
+                bitmap.SetPixel(coord_u, coord_v, color);
             }
         }
+        glTextureSubImage3D(id,
+                            0,          // mipmap level
+                            0,          // xoffset
+                            0,          // yoffset
+                            face,       // zoffset = face index
+                            face_size,  // width
+                            face_size,  // height
+                            1,          // depth = 1
+                            GL_RGB,     // format
+                            GL_UNSIGNED_BYTE,
+                            reinterpret_cast<const void*>(bitmap.Data())  // pointer to data
+        );
     }
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
 }
