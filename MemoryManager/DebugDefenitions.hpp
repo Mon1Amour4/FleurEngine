7dd8f9e79250883c8bf99d9284edf3a7251bfe3f
Oleg Cherkunov <mon1amour4@gmail.com>
2025-12-07 23:14:51 +0800

    [MemoryManager] remove memory defines

--- MemoryManager/DebugDefenitions.hpp
+++ MemoryManager/DebugDefenitions.hpp
@@ -93,9 +93,5 @@ struct MemoryInfo
 #define MM_PRINT(str) ((void)0);
 #endif
 
-#define II_NULL_INDEX 0xffffffff
-#define INVALID_OFFSET 0xFFFFFFFF
-#define LOCAL_HEAD(ptr, type) unsigned char* localHead = reinterpret_cast<unsigned char*>(ptr) + sizeof(type)
-#define TOCHARPTR(ptr) reinterpret_cast<unsigned char*>(ptr)
-#define CHUNK_STRIDE (sizeof(Chunk) + 4096)
+
 #pragma endregion
