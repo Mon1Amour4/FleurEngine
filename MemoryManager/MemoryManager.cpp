59b496b1a169000d5ea027775cc3f0061c07db03
Oleg Cherkunov <mon1amour4@gmail.com>
2025-12-14 02:12:38 +0800

    [TLSF] Remove extra debug info

--- MemoryManager/MemoryManager.cpp
+++ MemoryManager/MemoryManager.cpp
@@ -2,7 +2,7 @@
 
 #define WIN32_LEAN_AND_MEAN
 #include <Windows.h>
-size_t TLSFAllocator::id = 0;
+
 //======================================================================
 // Benchmark
 MM::Benchmark::Benchmark(float AvgPeriodSecs)
--- MemoryManager/TLSFAllocator.hpp
+++ MemoryManager/TLSFAllocator.hpp
@@ -5,6 +5,7 @@
 
 #define PS sizeof(char*)
 #define SIZE_TRASHHOLD 2032 + sizeof(used_block_header)
+#define MAX_VALUE 4'227'858'432
 
 struct TLSFAllocator
 {
@@ -13,9 +14,6 @@ struct TLSFAllocator
         // [0]F [1]T
         char bytes[2];
         int64_t _size;
-        size_t id;
-        uint8_t m_fli = 0;
-        uint8_t m_sli = 0;
         free_block_header* prev_phys_block;
 
         free_block_header* next_free;
@@ -58,9 +56,6 @@ struct TLSFAllocator
         // [1]T  1:last, 0:not last
         char bytes[2];
         int64_t _size;
-        size_t id;
-        uint8_t m_fli = 0;
-        uint8_t m_sli = 0;
         free_block_header* prev_phys_block;
 
         // 1 - free
@@ -94,7 +89,6 @@ struct TLSFAllocator
         }
     };
 
-    static size_t id;
     uint32_t m_FLI;
     uint32_t m_SLI;  // Power of two in range[1,32]
     const uint32_t m_MBS;
@@ -126,8 +120,6 @@ struct TLSFAllocator
     template <typename T, uint32_t ALign = 0>
     [[nodiscard]] T* allocate(uint32_t count = 1)
     {
-        uint32_t fliAfterSplit = 0;
-        uint32_t sliAfterSplit = 0;
         uint32_t firstIndex = 0, secondIndex = 0;
         used_block_header* foundBlock = nullptr;
         free_block_header* remainingBlock = nullptr;
@@ -135,57 +127,22 @@ struct TLSFAllocator
         size_t size = sizeof(T) * count + sizeof(used_block_header);
         uint32_t originalSize = size;
         foundBlock = get_block(size, &firstIndex, &secondIndex);
-        MM_DEBUG_BREAK(foundBlock->_size < originalSize)
-        uint32_t fliBeforeSplitting = firstIndex;
-        uint32_t sliBeforeSplitting = secondIndex;
-        uint32_t sizeAfterRounding = size;
 
+        MM_DEBUG_BREAK(foundBlock->_size < originalSize)
         MM_DEBUG_BREAK(foundBlock->_size == -1234)
         MM_DEBUG_BREAK(foundBlock->_size < originalSize)
 
-        bool isSplitted = false;
-        uint32_t blockFLI = firstIndex, blockSLI = secondIndex;
-        size_t debugOriginalSize = foundBlock->_size;
         if (foundBlock->_size - size > SIZE_TRASHHOLD)
         {
-            MM_DEBUG_BREAK(debugOriginalSize != foundBlock->_size)
+            MM_DEBUG_BREAK(foundBlock->_size < originalSize)
 
             remainingBlock = split(foundBlock, size);
-            MM_DEBUG_BREAK(foundBlock->_size > debugOriginalSize)
             mapping(remainingBlock->_size, &firstIndex, &secondIndex);
-            uint32_t checkSize = pow(2, firstIndex) + (secondIndex * pow(2, firstIndex - 5));
-            MM_DEBUG_BREAK(remainingBlock->_size < checkSize)
             insert(remainingBlock, firstIndex, secondIndex);
-
-            // Debug
-            MM_DEBUG_BREAK(blockFLI < firstIndex)
-            isSplitted = true;
-            // End of Debug
-
-
-            mapping(foundBlock->_size, &fliAfterSplit, &sliAfterSplit);
         }
         MM_DEBUG_BREAK(foundBlock->_size < originalSize)
+
         unsigned char* returnPtr = TOCHARPTR(foundBlock) + sizeof(used_block_header);
-
-        /* std::ofstream myFile;
-         myFile.open("MemorySnapshot.txt", std::ios_base::app);
-
-         char* buffer = new char[2048];
-         char* tmp = buffer;
-         tmp += sprintf(tmp, "TLSF allocation: ptr {0x%p} \n", (void*)returnPtr);
-         tmp += sprintf(tmp, "Found Block ID: %d, Original Size: %d, Size after rounding: %d, count: %d, block before split [%d][%d] -> [%d][%d]\n",
-                        foundBlock->id, originalSize, sizeAfterRounding, count, fliBeforeSplitting, sliBeforeSplitting, fliAfterSplit, sliAfterSplit);
-         if (isSplitted)
-         {
-             tmp += sprintf(tmp, "Block was splitted:New block size: %d, Remaining block: id: %d, {0x%p} %d [%d][%d]\n", debugOriginalSize, remainingBlock->id,
-                            (void*)remainingBlock, remainingBlock->_size, firstIndex, secondIndex);
-         }
-         tmp += '\0';
-
-         myFile << buffer;
-         myFile.close();
-         delete[] buffer;*/
         return reinterpret_cast<T*>(returnPtr);
     }
 
@@ -197,42 +154,12 @@ struct TLSFAllocator
         unsigned char* bytePtr = reinterpret_cast<unsigned char*>(ptr);
         free_block_header* block = reinterpret_cast<free_block_header*>(bytePtr - sizeof(used_block_header));
 
-        MM_DEBUG_BREAK(block->_size > 5'000'000 || block->_size == 0)
+        MM_DEBUG_BREAK(block->_size > MAX_VALUE || block->_size == 0)
 
         block->set_free();
 
-        // Debug
-        uint32_t mergedLeftSize = 0;
-        uint32_t mergedRightSize = 0;
-        uint32_t originalSize = block->_size;
-        uint32_t originalID = block->id;
-        bool mergedLeft = false;
-        bool mergeRight = false;
-        void* originalPtr = (void*)block;
-        void* leftPtr = nullptr;
-        void* rightPtr = nullptr;
-        // End debug
-
         free_block_header* mergedBlock = merge_left(block);
-
-        // Debug
-        if (mergedBlock != block)
-        {
-            mergedLeft = true;
-            mergedLeftSize = mergedBlock->_size;
-            leftPtr = (void*)mergedBlock;
-        }
-        // end debug
-
         merge_right(mergedBlock);
-
-        // debug
-        if ((!mergedLeft && mergedBlock->_size > originalSize) || (mergedLeftSize + originalSize > mergedBlock->_size))
-        {
-            mergeRight = true;
-            mergedRightSize = mergedBlock->_size;
-            rightPtr = mergedBlock;
-        }
 
         uint32_t firstIndex, secondIndex = 0;
         if (Fleur::Core::bit_scan_reverse(mergedBlock->_size, &firstIndex))
@@ -240,27 +167,6 @@ struct TLSFAllocator
             sli_no_rounding(mergedBlock->_size, &firstIndex, &secondIndex);
             free_block(mergedBlock, firstIndex, secondIndex);
         }
-
-        /*std::ofstream myFile;
-        myFile.open("MemorySnapshot.txt", std::ios_base::app);
-
-        char* buffer = new char[2048];
-        char* tmp = buffer;
-        tmp += sprintf(tmp, "TLSF dealloction: ptr {0x%p}, count: %d, Original block ID:%d, size: %d, original block {0x%d}\n", (void*)ptr, count, originalID,
-                       originalSize, originalPtr);
-        if (mergedLeft)
-        {
-            tmp += sprintf(tmp, "Block was merged left, size: %d, block ptr {0x%p}\n", mergedLeftSize, leftPtr);
-        }
-        if (mergeRight)
-        {
-            tmp += sprintf(tmp, "\nBlock was merged right, size: after merge: %d, right block ptr{0x%p}\n", mergedBlock, rightPtr);
-        }
-
-        tmp += '\0';
-        myFile << buffer;
-        myFile.close();
-        delete[] buffer;*/
     }
 
 private:
@@ -274,11 +180,6 @@ private:
 
     inline void SLI(uint32_t* size, uint32_t* fl, uint32_t* sl)
     {
-        // uint32_t subsize = 1u << (*fl - m_SLI);
-
-        // uint32_t rounded = (*size + subsize - 1) & ~(subsize - 1);
-
-        //*sl = (rounded - (1u << *fl)) / subsize;
         *size = *size + (1u << (*fl - m_SLI)) - 1;
         Fleur::Core::bit_scan_reverse(*size, fl);
         *sl = (*size >> (*fl - m_SLI)) - (1u << m_SLI);
@@ -318,7 +219,7 @@ private:
         free_block_header* currentBlock = m_FreeListHead[fli][sli];
         free_block_header* nextBlock = currentBlock->next_free;
 
-        MM_DEBUG_BREAK(currentBlock->_size < SIZE_TRASHHOLD || currentBlock->_size > 4227858432)
+        MM_DEBUG_BREAK(currentBlock->_size < SIZE_TRASHHOLD || currentBlock->_size > MAX_VALUE)
 
         m_FreeListHead[fli][sli] = currentBlock->next_free;
         if (nextBlock)
@@ -333,7 +234,7 @@ private:
         currentBlock->set_used();
         currentBlock->next_free = nullptr;
 
-        MM_DEBUG_BREAK(currentBlock->_size < SIZE_TRASHHOLD || currentBlock->_size > 4227858432)
+        MM_DEBUG_BREAK(currentBlock->_size < SIZE_TRASHHOLD || currentBlock->_size > MAX_VALUE)
 
         return reinterpret_cast<used_block_header*>(currentBlock);
     }
@@ -341,8 +242,6 @@ private:
     inline used_block_header* request_and_use_block(size_t size, uint32_t fli, uint32_t sli)
     {
         free_block_header* block = request_block(size);
-        block->id = id;
-        id++;
         insert(block, fli, sli);
         return use_block(fli, sli);
     }
@@ -411,19 +310,8 @@ private:
 
     inline free_block_header* split(used_block_header* usedBlock, uint32_t usedSize)
     {
-        // debug
-        MM_DEBUG_BREAK(usedBlock->_size <= usedSize)
-        size_t debugSize = usedBlock->_size - usedSize;
-        uint32_t usedBlockFLI = 0, usedBlockSLI = 0;
-        mapping(usedBlock->_size, &usedBlockFLI, &usedBlockSLI);
-        uint32_t remainingBlockFLI = 0, remainingBlockSLI = 0;
-        mapping(debugSize, &remainingBlockFLI, &remainingBlockSLI);
-        MM_DEBUG_BREAK(usedBlockFLI < remainingBlockFLI);
-        // end of debug
+        size_t remainingSize = usedBlock->_size - usedSize;
 
-        int remainingSize = usedBlock->_size - usedSize;
-
-        MM_DEBUG_BREAK(debugSize != remainingSize);
         MM_DEBUG_BREAK(remainingSize == -1234)
 
         usedBlock->_size = usedSize;
@@ -432,6 +320,7 @@ private:
         unsigned char* remainingBlockBytePtr = reinterpret_cast<unsigned char*>(usedBlock) + usedSize;
 
         MM_DEBUG_BREAK(remainingBlockBytePtr - reinterpret_cast<unsigned char*>(usedBlock) < SIZE_TRASHHOLD)
+
         free_block_header* remainingBlock = reinterpret_cast<free_block_header*>(remainingBlockBytePtr);
 
         remainingBlock->_size = remainingSize;
@@ -440,17 +329,15 @@ private:
         remainingBlock->prev_phys_block = reinterpret_cast<free_block_header*>(usedBlock);
         remainingBlock->next_free = nullptr;
         remainingBlock->prev_free = nullptr;
-        remainingBlock->id = id;
-        id++;
 
-        MM_DEBUG_BREAK(remainingBlock->_size < SIZE_TRASHHOLD)
-        MM_DEBUG_BREAK(remainingBlock->_size > 4227858432)
+        MM_DEBUG_BREAK(remainingBlock->_size < SIZE_TRASHHOLD || remainingBlock->_size > MAX_VALUE)
+
         return remainingBlock;
     }
 
     inline void insert(free_block_header* block, uint32_t fli, uint32_t sli)
     {
-        MM_DEBUG_BREAK(block->_size < SIZE_TRASHHOLD || block->_size > 4227858432)
+        MM_DEBUG_BREAK(block->_size < SIZE_TRASHHOLD || block->_size > MAX_VALUE)
 
         m_FL_Bitmap.SetBit(fli);
         m_SL_Bitmap[fli].SetBit(sli);
@@ -461,41 +348,31 @@ private:
         if (currentBlock)
             currentBlock->prev_free = block;
 
-        block->m_fli = fli;
-        block->m_sli = sli;
         m_FreeListHead[fli][sli] = block;
     }
 
     inline free_block_header* merge_left(free_block_header* currentBlock)
     {
         if (currentBlock->prev_phys_block)
-            MM_DEBUG_BREAK((uintptr_t)currentBlock->prev_phys_block < 2048)
+            MM_DEBUG_BREAK((uintptr_t)currentBlock->prev_phys_block < SIZE_TRASHHOLD)
 
         if (currentBlock->prev_phys_block && currentBlock->prev_phys_block->is_free())
         {
             free_block_header* prevBlock = currentBlock->prev_phys_block;
 
-            uint32_t prevBlockOGSize = prevBlock->_size;
-            uint32_t prevBlockOGSID = prevBlock->id;
-
             uint32_t fli = 0, sli = 0;
             mapping(prevBlock->_size, &fli, &sli);
-
-            MM_DEBUG_BREAK(fli != prevBlock->m_fli || sli != prevBlock->m_sli)
 
             use_block(fli, sli);
             prevBlock->set_free();
             prevBlock->prev_free = nullptr;
 
-            MM_DEBUG_BREAK(prevBlock->_size < SIZE_TRASHHOLD || prevBlock->_size > 4227858432)
-            MM_DEBUG_BREAK(currentBlock->_size < SIZE_TRASHHOLD || currentBlock->_size > 4227858432)
+            MM_DEBUG_BREAK(prevBlock->_size < SIZE_TRASHHOLD || prevBlock->_size > MAX_VALUE)
+            MM_DEBUG_BREAK(currentBlock->_size < SIZE_TRASHHOLD || currentBlock->_size > MAX_VALUE)
 
             prevBlock->_size += currentBlock->_size;
 
-            MM_DEBUG_BREAK(prevBlock->_size < SIZE_TRASHHOLD || prevBlock->_size > 4227858432)
-
-            prevBlock->id = id;
-            id++;
+            MM_DEBUG_BREAK(prevBlock->_size < SIZE_TRASHHOLD || prevBlock->_size > MAX_VALUE)
 
             if (!currentBlock->is_last_physical_block())
             {
@@ -520,22 +397,6 @@ private:
             currentBlock->prev_phys_block = nullptr;
             currentBlock->_size = -1234;
 
-            // Debug
-            /* std::ofstream myFile;
-             myFile.open("MemorySnapshot.txt", std::ios_base::app);
-
-             char* buffer = new char[2048];
-             char* tmp = buffer;
-             tmp += sprintf(tmp, "merge_left: currentBlock: {0x%p}, id: %d, size: %d \nWas merged with: {0x%p}, id: %d, size: %d", (void*)currentBlock,
-                            currentBlock->id, currentBlock->_size, (void*)prevBlock, prevBlockOGSID, prevBlockOGSize);
-             tmp += sprintf(tmp, " into: size: %d, id: %d\n", prevBlock->_size, prevBlock->id);
-             tmp += '\0';
-
-             myFile << buffer;
-             myFile.close();
-             delete[] buffer;*/
-            // End of debug
-
             return prevBlock;
         }
         else
@@ -547,38 +408,25 @@ private:
 
     inline void merge_right(free_block_header* block)
     {
-        uint32_t blockSize = block->_size;
-        uint32_t blockID = block->id;
-        uint32_t rightBlockID = 0;
-        uint32_t rightBlockSize = 0;
-
         if (!block->is_last_physical_block())
         {
             unsigned char* blockBytePtr = reinterpret_cast<unsigned char*>(block);
             free_block_header* rightBlock = reinterpret_cast<free_block_header*>(blockBytePtr + block->_size);
 
-            MM_DEBUG_BREAK(rightBlock->_size < SIZE_TRASHHOLD || rightBlock->_size > 4227858432)
+            MM_DEBUG_BREAK(rightBlock->_size < SIZE_TRASHHOLD || rightBlock->_size > MAX_VALUE)
 
             if (rightBlock->is_free())
             {
-                rightBlockID = rightBlock->id;
-                rightBlockSize = rightBlock->_size;
-
                 uint32_t fli = 0, sli = 0;
                 mapping(rightBlock->_size, &fli, &sli);
 
-                MM_DEBUG_BREAK(fli != rightBlock->m_fli || sli != rightBlock->m_sli)
-
                 use_block(fli, sli);
 
-                MM_DEBUG_BREAK(block->_size < SIZE_TRASHHOLD || block->_size > 4227858432)
+                MM_DEBUG_BREAK(block->_size < SIZE_TRASHHOLD || block->_size > MAX_VALUE)
 
                 block->_size += rightBlock->_size;
 
-                MM_DEBUG_BREAK(block->_size < SIZE_TRASHHOLD || block->_size > 4227858432)
-
-                rightBlock->id = id;
-                id++;
+                MM_DEBUG_BREAK(block->_size < SIZE_TRASHHOLD || block->_size > MAX_VALUE)
 
                 if (!rightBlock->is_last_physical_block())
                 {
@@ -597,22 +445,6 @@ private:
                 {
                     block->set_last_physical_block();
                 }
-
-                // Debug
-                /*std::ofstream myFile;
-                myFile.open("MemorySnapshot.txt", std::ios_base::app);
-
-                char* buffer = new char[2048];
-                char* tmp = buffer;
-                tmp += sprintf(tmp, "merge_right: currentBlock: {0x%p}, id: %d, size: %d \nWas merged with: {0x%p}, id: %d, size: %d", (void*)block, blockID,
-                               blockSize, (void*)rightBlock, rightBlockID, rightBlockSize);
-                tmp += sprintf(tmp, " into: size: %d, id: %d\n", block->_size, block->id);
-                tmp += '\0';
-
-                myFile << buffer;
-                myFile.close();
-                delete[] buffer;*/
-                // End of debug
             }
         }
 
