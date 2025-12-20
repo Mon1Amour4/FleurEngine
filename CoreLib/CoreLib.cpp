d071f5771ba622b5ab7fae753662054871356062
Oleg Cherkunov <mon1amour4@gmail.com>
2025-12-20 15:31:22 +0800

    Move FleurAllocator to CoreLib

--- CoreLib/CoreLib.cpp
+++ CoreLib/CoreLib.cpp
@@ -2,4 +2,11 @@
 // #include "DefaultAllocator.h"
 // #include "Sptr.h"
 // #include "Uptr.h"
-#include "singleton.hpp"
+#include "FleurAllocator.hpp"
+
+template <>
+Fleur::Memory::AllocAdapter& Fleur::singleton<Fleur::Memory::AllocAdapter>::instance()
+{
+    static Fleur::Memory::AllocAdapter inst;
+    return inst;
+}
--- Engine/Fleur/Application.h
+++ Engine/Fleur/Application.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <MemoryManager.h>
+
 #include "EventQueue.h"
 #include "Events/ApplicationEvent.h"
 #include "Events/EventVisitor.h"
@@ -21,45 +23,6 @@ class Texture;
 
 namespace Fleur
 {
-
-class FLEUR_API FleurAllocator : public singleton<FleurAllocator>
-{
-    friend class singleton<FleurAllocator>;
-
-    MM::MemoryManager* memory;
-
-public:
-    void Init(MM::MemoryManager* manager)
-    {
-        if (manager)
-            memory = manager;
-    }
-
-    template <class T, size_t Align = 0>
-    [[nodiscard]] T* allocate(uint32_t count = 1)
-    {
-        return memory->allocate<T, Align>(count);
-    }
-
-    template <class T>
-    void deallocate(void* ptr, uint32_t count)
-    {
-        memory->deallocate<int>(ptr, count);
-    }
-
-    template <typename T, size_t Align = 0, typename... Args>
-    [[nodiscard]] T* construct_at(Args&&... args)
-    {
-        return memory->construct_at<T, Align>(std::forward<Args>(args)...);
-    }
-
-    template <typename T, size_t Align = 0, typename... Args>
-    [[nodiscard]] T* construct_array_at(uint32_t count, Args&&... args)
-    {
-        return memory->construct_array_at<T, Align>(count, std::forward<Args>(args)...);
-    }
-};
-
 class FLEUR_API Application : public singleton<Application>
 {
     friend class singleton<Application>;
@@ -112,4 +75,5 @@ protected:
     Application();
     virtual ~Application() override;
 };
+
 }  // namespace Fleur
