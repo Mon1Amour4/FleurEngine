d3c65af39a092a5a8f4db55b5a35259c83ff098c
Oleg Cherkunov <mon1amour4@gmail.com>
2025-12-26 23:21:39 +0800

    Add VkShaderModule

--- Engine/Fleur/Renderer/Vulkan/Renderer_Vulkan.cpp
+++ Engine/Fleur/Renderer/Vulkan/Renderer_Vulkan.cpp
@@ -1,5 +1,8 @@
 #include "Renderer_Vulkan.h"
 
+// TODO REMOVE
+#include <fstream>
+
 #if defined(FLEUR_PLATFORM_WIN)
 #define NOMINMAX
 #include <Windows.h>
@@ -8,12 +11,12 @@
 
 #include <vulkan/vulkan.h>
 
-#include <iostream>
-#include <optional>
-#include <vector>
-#include <set>
 #include <algorithm>
+#include <iostream>
 #include <limits>
+#include <optional>
+#include <set>
+#include <vector>
 
 #if defined(FL_CONF_DEBUG)
 #define DBG_PRINT(moduleText, text) std::cout << moduleText << text << std::endl;
@@ -24,6 +27,7 @@
 #define MODULE
 #define DBG_PRINTM(text)
 #endif
+
 //======================================================================
 // Static functions
 static VKAPI_ATTR VkBool32 VKAPI_CALL debugCallback(VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity, VkDebugUtilsMessageTypeFlagsEXT messageType,
@@ -41,6 +45,23 @@ static VKAPI_ATTR VkBool32 VKAPI_CALL debugCallback(VkDebugUtilsMessageSeverityF
     }
 
     return VK_FALSE;
+}
+static std::vector<char> readFile(const std::string& filename)
+{
+    std::ifstream file(filename, std::ios::ate | std::ios::binary);
+
+    if (!file.is_open())
+    {
+        throw std::runtime_error("failed to open file!");
+    }
+
+    size_t fileSize = (size_t)file.tellg();
+    std::vector<char> buffer(fileSize);
+    file.seekg(0);
+    file.read(buffer.data(), fileSize);
+    file.close();
+
+    return buffer;
 }
 struct QueueFamilyIndices
 {
@@ -131,6 +152,9 @@ struct vulkanBackend::vulkanBackendImpl
 
     // Pipeline
     void createGraphicsPipeline();
+
+    // Shaders
+    VkShaderModule createShaderModule(const std::vector<char>& code);
 };
 
 //======================================================================
@@ -625,4 +649,44 @@ void vulkanBackend::vulkanBackendImpl::createImageViews()
 
 void vulkanBackend::vulkanBackendImpl::createGraphicsPipeline()
 {
+    auto vertShaderCode = readFile("C:/Engine/FleurEngine/Sandbox/Resources/Shaders/vert.spv");
+    auto fragShaderCode = readFile("C:/Engine/FleurEngine/Sandbox/Resources/Shaders/frag.spv");
+
+    VkShaderModule vertShaderModule = createShaderModule(vertShaderCode);
+    VkShaderModule fragShaderModule = createShaderModule(fragShaderCode);
+
+    // Pipeline Stage
+    VkPipelineShaderStageCreateInfo vertShaderStageInfo{};
+    vertShaderStageInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
+    vertShaderStageInfo.stage = VK_SHADER_STAGE_VERTEX_BIT;
+    vertShaderStageInfo.module = vertShaderModule;
+    vertShaderStageInfo.pName = "main";
+
+    VkPipelineShaderStageCreateInfo fragShaderStageInfo{};
+    fragShaderStageInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
+    fragShaderStageInfo.stage = VK_SHADER_STAGE_FRAGMENT_BIT;
+    fragShaderStageInfo.module = fragShaderModule;
+    fragShaderStageInfo.pName = "main";
+
+    VkPipelineShaderStageCreateInfo shaderStages[] = {vertShaderStageInfo, fragShaderStageInfo};
+
+    vkDestroyShaderModule(device, fragShaderModule, nullptr);
+    vkDestroyShaderModule(device, vertShaderModule, nullptr);
+}
+
+VkShaderModule vulkanBackend::vulkanBackendImpl::createShaderModule(const std::vector<char>& code)
+{
+    VkShaderModuleCreateInfo createInfo{};
+    createInfo.sType = VK_STRUCTURE_TYPE_SHADER_MODULE_CREATE_INFO;
+    createInfo.codeSize = code.size();
+    createInfo.pCode = reinterpret_cast<const uint32_t*>(code.data());
+
+    VkShaderModule shaderModule;
+    if (vkCreateShaderModule(device, &createInfo, nullptr, &shaderModule) != VK_SUCCESS)
+    {
+        DBG_PRINTM("Failed to create shader module!")
+        assert(false);
+    }
+
+    return shaderModule;
 }
