1d17fbda1d8e9b0c1bc09f6a758815e6d8475796
Oleg Cherkunov <mon1amour4@gmail.com>
2026-01-10 15:28:28 +0800

    Set uniform buffer descriptorsetlayout shaderstage as vertex

--- Engine/Fleur/Renderer/Vulkan/Renderer_Vulkan.cpp
+++ Engine/Fleur/Renderer/Vulkan/Renderer_Vulkan.cpp
@@ -1233,7 +1233,8 @@ void vulkanBackend::vulkanBackendImpl::createDescriptorSetLayout()
     uboLayoutBinding.binding = 0;
     uboLayoutBinding.descriptorType = VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER;
     uboLayoutBinding.descriptorCount = 1;
-    uboLayoutBinding.stageFlags = VK_SHADER_STAGE_FRAGMENT_BIT;;
+    uboLayoutBinding.stageFlags = VK_SHADER_STAGE_VERTEX_BIT;
+    ;
     uboLayoutBinding.pImmutableSamplers = nullptr;  // Optional
 
     VkDescriptorSetLayoutBinding samplerLayoutBinding{};
