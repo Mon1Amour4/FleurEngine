9cd5e84360ac60f819d20f13b99ad1981e01f191
Artem Zyktin <zyktin.a.a@gmail.com>
2026-02-04 15:17:39 +0800

    Add EntityId consts

--- Engine/Fleur/Ecs/EntityId.h
+++ Engine/Fleur/Ecs/EntityId.h
@@ -1,8 +1,13 @@
 #pragma once
 
 #include <cstdint>
+#include <limits>
 
 namespace Fleur::ECS
 {
-    using EntityId = uint32_t;
-}
+
+using EntityId = uint32_t;
+inline constexpr EntityId INVALID_ENTITY_ID = std::numeric_limits<EntityId>::max();
+inline constexpr EntityId MAX_ENTITY_ID = 1024 * 512; // 2^19
+
+} // namespace Fleur::ECS
