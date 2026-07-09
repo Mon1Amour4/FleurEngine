68ab8198d954eeabb2affc98066877cacdc6cd1f
Oleg Cherkunov <mon1amour4@gmail.com>
2026-07-09 14:28:03 +0800

    Add descriptor writer helper

--- Engine/Fleur/Lux/Vulkan/DescriptorPoolAllocator.cpp
+++ Engine/Fleur/Lux/Vulkan/DescriptorPoolAllocator.cpp
@@ -122,3 +122,60 @@ VkDescriptorSet vk::abstraction::DescriptorAllocator::allocate(VkDevice device, 
     m_ReadyPools.push_back(poolToUse);
     return ds;
 }
+
+// Writes a single buffer descriptor for `binding`.
+// Valid `type` values:
+// - VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER
+// - VK_DESCRIPTOR_TYPE_STORAGE_BUFFER
+// - VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER_DYNAMIC
+// - VK_DESCRIPTOR_TYPE_STORAGE_BUFFER_DYNAMIC
+//
+// `binding` must exist in the target descriptor set layout and be compatible with `type`.
+// `buffer` must be a valid VkBuffer.
+// `offset` and `size` specify the buffer range exposed through the descriptor.
+void vk::abstraction::DescriptorWriter::write_buffer(int binding, VkBuffer buffer, size_t size, size_t offset, VkDescriptorType type)
+{
+    VkDescriptorBufferInfo& info = bufferInfos.emplace_back(VkDescriptorBufferInfo{.buffer = buffer, .offset = offset, .range = size});
+
+    VkWriteDescriptorSet write = {.sType = VK_STRUCTURE_TYPE_WRITE_DESCRIPTOR_SET};
+
+    write.dstBinding = binding;
+    write.dstSet = VK_NULL_HANDLE;  // left empty for now until we need to write it
+    write.descriptorCount = 1;
+    write.descriptorType = type;
+    write.pBufferInfo = &info;
+
+    writes.push_back(write);
+}
+
+void vk::abstraction::DescriptorWriter::write_image(int binding, VkImageView image, VkSampler sampler, VkImageLayout layout, VkDescriptorType type)
+{
+    VkDescriptorImageInfo& info = imageInfos.emplace_back(VkDescriptorImageInfo{.sampler = sampler, .imageView = image, .imageLayout = layout});
+
+    VkWriteDescriptorSet write = {.sType = VK_STRUCTURE_TYPE_WRITE_DESCRIPTOR_SET};
+
+    write.dstBinding = binding;
+    write.dstSet = VK_NULL_HANDLE;  // left empty for now until we need to write it
+    write.descriptorCount = 1;
+    write.descriptorType = type;
+    write.pImageInfo = &info;
+
+    writes.push_back(write);
+}
+
+void vk::abstraction::DescriptorWriter::clear()
+{
+    imageInfos.clear();
+    writes.clear();
+    bufferInfos.clear();
+}
+
+void vk::abstraction::DescriptorWriter::update_set(VkDevice device, VkDescriptorSet set)
+{
+    for (VkWriteDescriptorSet& write : writes)
+    {
+        write.dstSet = set;
+    }
+
+    vkUpdateDescriptorSets(device, (uint32_t)writes.size(), writes.data(), 0, nullptr);
+}
--- Engine/Fleur/Lux/Vulkan/DescriptorPoolAllocator.h
+++ Engine/Fleur/Lux/Vulkan/DescriptorPoolAllocator.h
@@ -2,6 +2,7 @@
 
 #include <vulkan/vulkan.h>
 
+#include <queue>
 #include <span>
 #include <vector>
 
@@ -31,6 +32,19 @@ private:
     std::vector<PoolSizeRatio> m_Ratios;
     std::vector<VkDescriptorPool> m_FullPulls;
     std::vector<VkDescriptorPool> m_ReadyPools;
+};
+
+struct DescriptorWriter
+{
+    std::deque<VkDescriptorImageInfo> imageInfos;
+    std::deque<VkDescriptorBufferInfo> bufferInfos;
+    std::vector<VkWriteDescriptorSet> writes;
+
+    void write_image(int binding, VkImageView image, VkSampler sampler, VkImageLayout layout, VkDescriptorType type);
+    void write_buffer(int binding, VkBuffer buffer, size_t size, size_t offset, VkDescriptorType type);
+
+    void clear();
+    void update_set(VkDevice device, VkDescriptorSet set);
 };
 
 }  // namespace vk::abstraction
